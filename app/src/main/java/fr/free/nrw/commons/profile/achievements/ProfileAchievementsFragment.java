9d94ca9391d262ccd5d4e1ef9a7525f4f9f60b77
unknown <silkypriya64@gmail.com>
2019-05-28 00:32:23 +0530

    squash

--- app/src/main/java/fr/free/nrw/commons/profile/achievements/ProfileAchievementsFragment.java
+++ app/src/main/java/fr/free/nrw/commons/profile/achievements/ProfileAchievementsFragment.java
@@ -1,49 +1,40 @@
-package fr.free.nrw.commons.achievements;
+package fr.free.nrw.commons.profile.achievements;
 
 import android.accounts.Account;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.FileProvider;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.dinuscxj.progressbar.CircleProgressBar;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Objects;
-
 import javax.inject.Inject;
-
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import dagger.android.support.DaggerFragment;
 import fr.free.nrw.commons.R;
 import fr.free.nrw.commons.Utils;
 import fr.free.nrw.commons.auth.SessionManager;
 import fr.free.nrw.commons.mwapi.OkHttpJsonApiClient;
-import fr.free.nrw.commons.theme.NavigationBaseActivity;
 import fr.free.nrw.commons.utils.ViewUtil;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -51,14 +42,10 @@ import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
 
-
-/**
- * activity for sharing feedback on uploaded activity
- */
-public class AchievementsActivity extends NavigationBaseActivity {
+public class ProfileAchievementsFragment extends DaggerFragment {
 
     private static final double BADGE_IMAGE_WIDTH_RATIO = 0.4;
-    private static final double BADGE_IMAGE_HEIGHT_RATIO = 0.3;
+    private static final double BADGE_IMAGE_HEIGHT_RATIO = 0.27;
 
     private LevelController.LevelInfo levelInfo;
 
@@ -68,8 +55,6 @@ public class AchievementsActivity extends NavigationBaseActivity {
     TextView badgeText;
     @BindView(R.id.achievement_level)
     TextView levelNumber;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
     @BindView(R.id.thanks_received)
     TextView thanksReceived;
     @BindView(R.id.images_uploaded_progressbar)
@@ -101,31 +86,40 @@ public class AchievementsActivity extends NavigationBaseActivity {
     @BindView(R.id.wikidata_edits)
     TextView wikidataEditsText;
 
-
     @Inject
     SessionManager sessionManager;
     @Inject
     OkHttpJsonApiClient okHttpJsonApiClient;
-    MenuItem item;
 
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     /**
-     * This method helps in the creation Achievement screen and
-     * dynamically set the size of imageView
-     *
-     * @param savedInstanceState Data bundle
+     * Create an instance of the fragment with the right bundle parameters
+     * @return an instance of the fragment
      */
+    public static ProfileAchievementsFragment newInstance() {
+        return new ProfileAchievementsFragment();
+    }
+
     @Override
-    @SuppressLint("StringFormatInvalid")
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_achievements);
-        ButterKnife.bind(this);
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View v = inflater.inflate(R.layout.fragment_profile_achievements, container, false);
+        ButterKnife.bind(this, v);
+        setHasOptionsMenu(true);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         // DisplayMetrics used to fetch the size of the screen
         DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int height = displayMetrics.heightPixels;
         int width = displayMetrics.widthPixels;
 
@@ -136,13 +130,11 @@ public class AchievementsActivity extends NavigationBaseActivity {
         params.width = (int) (width * BADGE_IMAGE_WIDTH_RATIO);
         imageView.requestLayout();
 
-        setSupportActionBar(toolbar);
         progressBar.setVisibility(View.VISIBLE);
 
         hideLayouts();
         setAchievements();
         setWikidataEditCount();
-        initDrawer();
     }
 
     @Override
@@ -158,51 +150,6 @@ public class AchievementsActivity extends NavigationBaseActivity {
     public void showInfoDialog(){
         launchAlert(getResources().getString(R.string.Achievements)
                 ,getResources().getString(R.string.achievements_info_message));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_about, menu);
-        item=menu.getItem(0);
-        item.setVisible(false);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.share_app_icon) {
-            View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
-            Bitmap screenShot = Utils.getScreenShot(rootView);
-            showAlert(screenShot);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * To take bitmap and store it temporary storage and share it
-     * @param bitmap
-     */
-    void shareScreen(Bitmap bitmap) {
-        try {
-            File file = new File(this.getExternalCacheDir(), "screen.png");
-            FileOutputStream fOut = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
-            fOut.flush();
-            fOut.close();
-            file.setReadable(true, false);
-            Uri fileUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName()+".provider", file);
-            grantUriPermission(getPackageName(), fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.putExtra(Intent.EXTRA_STREAM, fileUri);
-            intent.setType("image/png");
-            startActivity(Intent.createChooser(intent, "Share image via"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     /**
@@ -257,7 +204,7 @@ public class AchievementsActivity extends NavigationBaseActivity {
 
     private void showSnackBarWithRetry() {
         progressBar.setVisibility(View.GONE);
-        ViewUtil.showDismissibleSnackBar(findViewById(android.R.id.content),
+        ViewUtil.showDismissibleSnackBar(getView().findViewById(android.R.id.content),
                 R.string.achievements_fetch_failed, R.string.retry, view -> setAchievements());
     }
 
@@ -265,7 +212,7 @@ public class AchievementsActivity extends NavigationBaseActivity {
      * Shows a generic error toast when error occurs while loading achievements or uploads
      */
     private void onError() {
-        ViewUtil.showLongToast(this, getResources().getString(R.string.error_occurred));
+        ViewUtil.showLongToast(getActivity(), getResources().getString(R.string.error_occurred));
         progressBar.setVisibility(View.GONE);
     }
 
@@ -315,7 +262,7 @@ public class AchievementsActivity extends NavigationBaseActivity {
     }
 
     private void setZeroAchievements() {
-        AlertDialog.Builder builder=new AlertDialog.Builder(this)
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity())
                 .setMessage("You haven't made any contributions yet")
                 .setPositiveButton("Ok", (dialog, which) -> {
                 });
@@ -359,7 +306,7 @@ public class AchievementsActivity extends NavigationBaseActivity {
         levelUpInfoString += " " + levelInfo.getLevelNumber();
         levelNumber.setText(levelUpInfoString);
         imageView.setImageDrawable(VectorDrawableCompat.create(getResources(), R.drawable.badge,
-                new ContextThemeWrapper(this, levelInfo.getLevelStyle()).getTheme()));
+                new ContextThemeWrapper(getActivity(), levelInfo.getLevelStyle()).getTheme()));
         badgeText.setText(Integer.toString(levelInfo.getLevelNumber()));
     }
 
@@ -368,7 +315,7 @@ public class AchievementsActivity extends NavigationBaseActivity {
      * @param context
      */
     public static void startYourself(Context context) {
-        Intent intent = new Intent(context, AchievementsActivity.class);
+        Intent intent = new Intent(context, ProfileAchievementsFragment.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(intent);
     }
@@ -385,7 +332,6 @@ public class AchievementsActivity extends NavigationBaseActivity {
             setUploadProgress(achievements.getImagesUploaded());
             setImageRevertPercentage(achievements.getNotRevertPercentage());
             progressBar.setVisibility(View.GONE);
-            item.setVisible(true);
             layoutImageReverts.setVisibility(View.VISIBLE);
             layoutImageUploaded.setVisibility(View.VISIBLE);
             layoutImageUsedByWiki.setVisibility(View.VISIBLE);
@@ -405,24 +351,6 @@ public class AchievementsActivity extends NavigationBaseActivity {
         layoutStatistics.setVisibility(View.INVISIBLE);
         imageView.setVisibility(View.INVISIBLE);
         levelNumber.setVisibility(View.INVISIBLE);
-    }
-
-    /**
-     * It display the alertDialog with Image of screenshot
-     * @param screenshot
-     */
-    public void showAlert(Bitmap screenshot){
-        AlertDialog.Builder alertadd = new AlertDialog.Builder(AchievementsActivity.this);
-        LayoutInflater factory = LayoutInflater.from(AchievementsActivity.this);
-        final View view = factory.inflate(R.layout.image_alert_layout, null);
-        ImageView screenShotImage = view.findViewById(R.id.alert_image);
-        screenShotImage.setImageBitmap(screenshot);
-        TextView shareMessage = view.findViewById(R.id.alert_text);
-        shareMessage.setText(R.string.achievements_share_message);
-        alertadd.setView(view);
-        alertadd.setPositiveButton(R.string.about_translate_proceed, (dialog, which) -> shareScreen(screenshot));
-        alertadd.setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.cancel());
-        alertadd.show();
     }
 
     @OnClick(R.id.images_upload_info)
@@ -467,7 +395,7 @@ public class AchievementsActivity extends NavigationBaseActivity {
      * @param message
      */
     private void launchAlert(String title, String message){
-        new AlertDialog.Builder(AchievementsActivity.this)
+        new AlertDialog.Builder(getActivity())
                 .setTitle(title)
                 .setMessage(message)
                 .setCancelable(true)
@@ -484,11 +412,12 @@ public class AchievementsActivity extends NavigationBaseActivity {
         Account currentAccount = sessionManager.getCurrentAccount();
         if (currentAccount == null) {
             Timber.d("Current account is null");
-            ViewUtil.showLongToast(this, getResources().getString(R.string.user_not_logged_in));
-            sessionManager.forceLogin(this);
+            ViewUtil.showLongToast(getActivity(), getResources().getString(R.string.user_not_logged_in));
+            sessionManager.forceLogin(getActivity());
             return false;
         }
         return true;
     }
 
 }
+
